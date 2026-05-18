309dde251e93b7a46142c12a11800c842220aa29
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2026-05-18 07:35:18 +0200

    Point to the branch

--- terraform/azure/foundation/main.tf
+++ terraform/azure/foundation/main.tf
@@ -1,3 +1,3 @@
 module "foundation" {
-  source = "github.com/go-euc/lab-solution/src/terraform/azure/foundation?ref=v2605.17.13-dev"
+  source = "github.com/go-euc/lab-solution//src/terraform/azure/foundation?ref=features/avd"
 }
